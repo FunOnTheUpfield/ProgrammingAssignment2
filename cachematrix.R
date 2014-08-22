bc9d2e5831b8b01963c613ef1f0b4acfa4300c3a
FunOnTheUpfield <bikenerd@gmail.com>
2014-08-22 15:50:18 +1000

    Now with more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+# by FunOnTheUpfield
 
-## Write a short comment describing this function
+## Provinance
+## This is based on code available from:
+## https://github.com/rdpeng/ProgrammingAssignment2
+## Additional information available in the README.md 
 
 
+## The changes involve minor substitutions
 ## I have assuemed this is a search and replace exercise,
 ## with the "Mean" function replaced by "inverse" function
 ## I could find no test data to validate this code...
 
-## This work makes the following subsitutions:
+## This work makes the following subsitutions from the orignal code:
 ## Replace 'makeVector' with 'makeCacheMatrix';  ;'numeric' with 'matrix', 'm' with 'inv'
-## Replace 'setMean' and 'getMean' with 'setInverse' and 'getInverse'
 
+##  ---
+
+## Overview
+## This function creates the inverse of a matrix
+## As this can be time consuming it also stores the value of the calcuation 
+## so can be retrieved without recalculating it. 
+
+
+
+## makeCacheMatrix - This function creates a special "matrix" object
+## The object inlcudes space to store its inverse (when calculated).
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -26,7 +40,10 @@ makeCacheMatrix <- function(x = matrix()) {
        setinverse = setinverse,
        getinverse = getinverse)
 }
-  ## Use example in question. Replace "numeric" with "matrix" and "mean" with "inverse"
+
+## cacheSolve checks to see if the inverse has already been calculated
+## If it hasn't, it does the inverse calculation
+## If it has it returns the previously calculated value. 
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
@@ -40,4 +57,6 @@ cacheSolve <- function(x, ...) {
   inv
 }
 
-## Similarly use example. Replace "m" with "inv",
+## Validation Status
+## This code is untested. 
+
